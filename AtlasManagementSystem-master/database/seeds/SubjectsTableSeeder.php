6c0750e4cc0c8e7c512430182a2344c435851421
taisei2002 <104011592+taisei2002@users.noreply.github.com>
2022-12-17 21:58:50 +0900

    初期値 設定　/ subject

--- AtlasManagementSystem-master/database/seeds/SubjectsTableSeeder.php
+++ AtlasManagementSystem-master/database/seeds/SubjectsTableSeeder.php
@@ -13,5 +13,26 @@ class SubjectsTableSeeder extends Seeder
     public function run()
     {
         // 国語、数学、英語を追加
+        DB::table('subjects')->insert([
+            [
+                'id' => 1,
+                'subject' => '国語',
+
+            ],
+
+             [
+                'id' => 2,
+                'subject' => '数学',
+
+             ],
+
+              [
+                'id' => 3,
+                'subject' => '英語',
+
+              ],
+
+            ]);
+
     }
 }
